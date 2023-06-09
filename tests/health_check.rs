f198de4ed82a12f850c182836d0412a31c3ec024
Tyler Weaver <tyler@picknik.ai>
2023-06-09 16:06:19 -0600

    health_check endpoint

---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
