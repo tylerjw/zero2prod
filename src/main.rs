cb3f11e0b265362ebcb5d78555c59a9354f5b84a
Tyler Weaver <tyler@picknik.ai>
2023-06-13 12:50:43 -0600

    Now with a database
    Signed-off-by: Tyler Weaver <tyler@picknik.ai>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use zero2prod::run;
+use sqlx::PgPool;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = std::net::TcpListener::bind(address)?;
+    run(listener, connection_pool)?.await
 }
